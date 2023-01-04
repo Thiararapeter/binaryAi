b1ed0e05e0c0741d1e18d3244b44ced9ecc85e7e
Thiarara <contact@thiarara.co.ke>
2023-01-04 09:49:36 +0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handlesubmit = async (e) => {
 
     //fetch data from ther serever -> bot response
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://binary-zdb8.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
